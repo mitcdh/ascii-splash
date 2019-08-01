9479707ca56338ccbcd85df1d1492dffaf25a1a8
Mitchell Hewes <me@mitcdh.com>
2019-08-01 19:23:40 +0200

    Usability updates

--- ascii-splash.py
+++ ascii-splash.py
@@ -1,19 +1,25 @@
-from tkinter import *
-from threading import Thread
-import socketserver
-import queue
 import logging
 import os
+import queue
+import socketserver
+import threading
+import tkinter
 
-# logging configuration
+## logging configuration
 logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s',
                     level=os.environ.get("LOG_LEVEL", "DEBUG"))
-CMDQ = queue.Queue(maxsize=0)
 
+
+## Server related configuration options
 LISTEN_HOST = os.environ.get('LISTEN_HOST', '127.0.0.1')
 LISTEN_PORT = int(os.environ.get('LISTEN_PORT', '1337'))
-LISTEN_SECRET = b'UNCONTAINED'
-ESCAPE_CODE = "<Escape>."
+LISTEN_SECRET = os.environ.get('LISTEN_SECRET', 'UNCONTAINED')
+
+## Splash screen related configuration
+SPLASH_ESCAPE = "<Escape>."
+SPLASH_BG_COLOUR = "red"
+SPLASH_FG_COLOUR = "white"
+SPLASH_FONT = "Courier"
 SPLASH_MESSAGE = """
             $uuuu$$$$$uuuuuuu$           
              $uuuuuu$$uu$uuu$            
@@ -42,67 +48,78 @@ $$$$'''$$$$$$$$$$uuu   uu$$$$$$$$$'''$$$'
      $$$'       C0NTA1N3D        $$$$'   
 """
 
+# Creating global queue for communication between Server and Splasher
+CMDQ = queue.Queue(maxsize=0)
 
 
-class Splasher(Thread):
+class Worker(threading.Thread):
     def __init__(self):
-        super(Splasher, self).__init__(name="Splasher")
+        super(Worker, self).__init__(name="Worker")
+        logging.info("WKR: created splash Worker thread")
+
+    def run(self):
+        while True:
+            logging.debug("WKR: creating new Splasher instance")
+            self.splash = Splasher()
+
+class Splasher():
+    def __init__(self):
+        self.root = tkinter.Tk()
+        self.root.withdraw()
+        self.root.after(0, self.parse)
+        self.root.mainloop()
 
     def parse(self):
         try:
             cmd = CMDQ.get(block=False)
-            logging.debug("SPL: removed (%s) from queue" % (cmd))
+            logging.debug("SPL: removed '%s' from queue" % (cmd))
             if (cmd == LISTEN_SECRET):
-                self.ascii_splash()
+                logging.debug("SPL: listen secret '%s' matched" % (cmd))
+                self.splash()
             CMDQ.task_done()
         except queue.Empty:
             pass
         self.root.after(500, self.parse)
 
-    def run(self):
-        self.root = Tk()
-        self.root.withdraw()
-        self.root.after(0, self.parse)
-        self.root.mainloop()
+    def splash(self):
+        logging.debug("SPL: creating new splash window")
+        window = tkinter.Toplevel(self.root)
 
-    def ascii_splash(self):
-        logging.debug("SPL: generating new splash window")
-        splash_screen = Toplevel(self.root)
+        window.attributes("-topmost", True)
+        window.attributes('-fullscreen', True)
+        window.bind(SPLASH_ESCAPE, lambda w: w.widget.destroy())
+        window.focus_force()
+        window.lift()
 
-        splash_screen.attributes("-topmost", True)
-        splash_screen.attributes('-fullscreen', True)
-        splash_screen.bind(ESCAPE_CODE, lambda w: w.widget.destroy())  # Exit on Escape + period
-        splash_screen.focus_force()
-        splash_screen.lift()
+        label = tkinter.Label(window, text=SPLASH_MESSAGE)
+        label.config(bg=SPLASH_BG_COLOUR, fg=SPLASH_FG_COLOUR,
+                     font=(SPLASH_FONT, 18))
+        label.pack(side=tkinter.TOP, expand=tkinter.YES,
+                   fill=tkinter.BOTH)
 
-        ascii_label = Label(splash_screen, text=SPLASH_MESSAGE)
-        ascii_label.config(bg="red", fg="white", font=('Courier', 18))
-        ascii_label.pack(side=TOP, expand=YES, fill=BOTH)
-
-class SplashListener(socketserver.StreamRequestHandler):
+class Server(socketserver.StreamRequestHandler):
     def handle(self):
-        cmd = self.rfile.readline().strip()
+        cmd = self.rfile.readline().decode().strip()
         CMDQ.put(cmd)
-        logging.debug("SRV: client %s added (%s) to queue" % (self.client_address[0], cmd))
+        logging.debug("SRV: client '%s' added '%s' to queue"
+                      % (self.client_address[0], cmd))
 
-try:
-    tk_worker = Splasher()
-    tk_worker.setDaemon(True)
-    tk_worker.start()
-except Exception as e:
-    logging.error("SPL: failed to start Splash Worker thread (%s)"
-                  % (e))
-    raise
-else:
-    logging.info("SPL: successfully created Splash Worker thread")
+if __name__ == "__main__":
+    try:
+        tk_worker = Worker()
+        tk_worker.setDaemon(True)
+        tk_worker.start()
+    except Exception as e:
+        logging.error("SPL: failed to start Splash Worker thread '%s'"
+                      % (e))
+        raise
 
-try:
-    server = socketserver.TCPServer((LISTEN_HOST, LISTEN_PORT), SplashListener)
-    logging.info("SRV: starting splash server on %s:%s triggering on '%s'" % (LISTEN_HOST, LISTEN_PORT, LISTEN_SECRET))
-    server.serve_forever(poll_interval=0.5)
-except IOError as e:
-    logging.error("SRV: IO exception encountered while starting secret splash server (%s) shutting down"
-                  % (e))
-    CMDQ.join()
-    tk_worker.stop()
-    raise
+    try:
+        server = socketserver.TCPServer((LISTEN_HOST, LISTEN_PORT), Server)
+        logging.info("SRV: starting Server on '%s:%s' triggering on secret '%s'"
+                     % (LISTEN_HOST, LISTEN_PORT, LISTEN_SECRET))
+        server.serve_forever(poll_interval=0.5)
+    except IOError as e:
+        logging.error("SRV: IO exception encountered while starting server '%s'"
+                      % (e))
+        raise
